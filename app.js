be0a58fa53574f324d5e387ebcebc0d6bc808fb5
Sarah E <seatherly.prsvr@gmail.com>
2023-12-13 17:07:51 -0600

    try again

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.get('/', function (req, res) {
 
 
 
-const port = 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+app.listen(3000, ()=>{
+  console.log('Server started on Port: 3000');
+})
